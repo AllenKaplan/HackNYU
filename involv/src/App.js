4caca4cd8aee85d8ef0a929fdba104acd2a7992e
dyampols <daria.yampolsky@gmail.com>
2019-02-16 18:22:19 -0500

    daria'd it

--- involv/src/App.js
+++ involv/src/App.js
@@ -1,28 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import LoginBox from './LoginBox.js';
+import RegisterBox from './RegisterBox.js';
+import reactDOM from 'react-dom';
+import './formating.css';
 
-class App extends Component {
+
+class App extends React.Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoginOpen: true,
+      isRegisterOpen: false
+    };
+  }
+
+  showLoginBox() {
+    this.setState({isLoginOpen: true, isRegisterOpen: false});
+  }
+
+  showRegisterBox() {
+    this.setState({isRegisterOpen: true, isLoginOpen: false});
+  }
+  
   render() {
     return (
+      
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="root-container">
+   
+        
+     
+        <div className="box-container">
+        
+        {/* { <LoginBox/>}
+          {<RegisterBox/>} */}
+          {this.state.isLoginOpen && <LoginBox/>}
+          {this.state.isRegisterOpen && <RegisterBox/>}
+        </div>
+        <div className="box-controller">
+       <div
+         className={"controller " + (this.state.isLoginOpen
+         ? "selected-controller"
+         : "")}
+         onClick={this
+         .showLoginBox
+         .bind(this)}>
+         Login
+       </div>
+       <div
+         className={"controller " + (this.state.isRegisterOpen
+         ? "selected-controller"
+         : "")}
+         onClick={this
+         .showRegisterBox
+         .bind(this)}>
+         Register
+       </div>
+     </div>
+     </div>
+        </div>
     );
+
+   
   }
 }
+// app.get('formating.css', function(req, res){ res.send('formating.css'); res.end(); });
+//    ;
 
+
+reactDOM.render(
+    <App />, document.getElementById("root"));
 export default App;
