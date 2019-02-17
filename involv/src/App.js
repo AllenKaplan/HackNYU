dfc3a6478b3d4bbee4a831fe24253790d42373f8
dyampols <daria.yampolsky@gmail.com>
2019-02-17 00:27:32 -0500

    home?

--- involv/src/App.js
+++ involv/src/App.js
@@ -10,7 +10,6 @@ import home from './home.js'
 import {Router, Route, browserHistory} from "react-router"
 //this.props.router.push('/login')
 
-<<<<<<< HEAD
 
 class App extends React.Component {
   render() {
@@ -22,69 +21,6 @@ class App extends React.Component {
 
         <Route path={"interests"} component={Interests} />
       </Router>
-=======
-class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-    this.setState({ responseToPost: body });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
-      </div>
->>>>>>> allen
     );
 
    
--- involv/src/home.js
+++ involv/src/home.js
@@ -9,6 +9,7 @@ import {Router, Route, browserHistory} from "react-router"
 //import { Searchbar } from 'react-native-paper';
 import ReactSearchBox from 'react-search-box'
 import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
 import Button from 'react-bootstrap/Button'
 
 //import ToggleButton from 'react-bootstrap/ca'
@@ -68,9 +69,9 @@ Welcome, Daria
       <div className="header"> 
         These extracuricular activities are suggested for you
       </div>
-        <div className="cardgroup">
+        {/* <div className="cardgroup">
         <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/171x180" roundedCircle />
+  <Card.Img variant="top" src="https://3c1703fe8d.site.internapcdn.net/newman/gfx/news/hires/2017/chess.jpg" roundedCircle />
   <Card.Body>
     <Card.Title>Chess Club</Card.Title>
     <Card.Text>
@@ -80,8 +81,60 @@ Welcome, Daria
   </Card.Body>
 </Card>;
 </div>
+<div className="cardgroup">
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://yt3.ggpht.com/a-/AN66SAxyqDCNY-64KzZtyG7UVkdVOP-AZucSGVgP3A=s900-mo-c-c0xffffffff-rj-k-no" roundedCircle />
+  <Card.Body>
+    <Card.Title>DECA</Card.Title>
+    <Card.Text>
+      Thornhill Seconday School - DECA, DECA, DECA! Meetings every Thursday from 4:30PM-7PM
+    </Card.Text>
+    <Button variant="primary">Learn More</Button>
+  </Card.Body>
+</Card>;
+</div> */}
+
+<CardDeck>
+  <Card>
+    <Card.Img variant="top" src="https://cdn11.bigcommerce.com/s-dlmdd/images/stencil/500x200/products/300/8488/0140-basic-chess-pieces__18529.1496417994.jpg?c=2" />
+    <Card.Body>
+      <Card.Title>Chess Club</Card.Title>
+      <Card.Text>
+      Thornhill Secondary School - Join us every Tuesday from 5PM-7PM
+      </Card.Text>
+    </Card.Body>
+    {/* <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer> */}
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="https://www.gannett-cdn.com/-mm-/814d8e115acc08ac2cd65e46a47370886a13c7db/c=167-0-834-667/local/-/media/2018/02/06/Pensacola/Pensacola/636535542769028532-sm2018-0206-gulf-breeze-arnold-soccer-1-3a-quarterfinal-18.jpg?width=200&height=200&fit=crop" />
+    <Card.Body>
+      <Card.Title>Thornhill Soccer Club</Card.Title>
+      <Card.Text>
+      Thornhill Soccer Club - Our Select team is looking for new team members. Try outs will be heald on March 3rd come out and try out!
+      </Card.Text>
+    </Card.Body>
+    {/* <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer> */}
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="https://d3e1o4bcbhmj8g.cloudfront.net/photos/715402/big_square/c742e490d77772e5d7e538878400eeaa925b358f.jpg"  />
+    <Card.Body>
+      <Card.Title>Reach for the Top</Card.Title>
+      <Card.Text>
+        Thornhill Secondary School - Reach for the Top is a trivia club! We want you to join. We meet Mondays from 6:00PM-8:00PM
+      </Card.Text>
+    </Card.Body>
+    {/* <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer> */}
+  </Card>
+</CardDeck>;
 </div>
-  
+
+
     );
   }
 }
